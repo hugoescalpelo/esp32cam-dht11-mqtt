f11f377072bfa996403b4b659a987e8507ffc37b
Hugo Escalpelo <hugoescalpelo@gmail.com>
2022-06-06 17:03:45 -0500

    Update DHT11-MQTT-ESP32CAM.ino

--- DHT11-MQTT-ESP32CAM/DHT11-MQTT-ESP32CAM.ino
+++ DHT11-MQTT-ESP32CAM/DHT11-MQTT-ESP32CAM.ino
@@ -156,7 +156,7 @@ void callback(char* topic, byte* message, unsigned int length) {
 
   // Ejemplo, en caso de recibir el mensaje true - false, se cambiará el estado del led soldado en la placa.
   // El ESP323CAM está suscrito al tema esp/output
-  if (String(topic) == "esp32/output") {  // En caso de recibirse mensaje en el tema esp32/output
+  if (String(topic) == "codigoIoT/SIC/G5/led") {  // En caso de recibirse mensaje en el tema esp32/output
     if(messageTemp == "true"){
       Serial.println("Led encendido");
       digitalWrite(flashLedPin, HIGH);
@@ -176,7 +176,7 @@ void reconnect() {
     // Intentar reconexión
     if (client.connect("ESP32CAMClient")) { //Pregunta por el resultado del intento de conexión
       Serial.println("Conectado");
-      client.subscribe("esp32/output"); // Esta función realiza la suscripción al tema
+      client.subscribe("codigoIoT/SIC/G5/led"); // Esta función realiza la suscripción al tema
     }// fin del  if (client.connect("ESP32CAMClient"))
     else {  //en caso de que la conexión no se logre
       Serial.print("Conexion fallida, Error rc=");
